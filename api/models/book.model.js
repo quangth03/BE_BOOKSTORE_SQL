f679ecedc54fd318d5efa9a2e6234239793eabf5
quangth03 <quangth2201@gmail.com>
2024-11-12 01:10:49 +0700

    quantity controller

--- api/models/book.model.js
+++ api/models/book.model.js
@@ -1,38 +1,41 @@
 module.exports = (sequelize, Sequelize, DataTypes) => {
-    const Book = sequelize.define(
-      "book", // Model name
-      {
-        // Model attributes
-        id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true
-        },
-        title: {
-          type: DataTypes.STRING
-        },
-        author: {
-          type: DataTypes.STRING
-        },
-        price:{
-            type: DataTypes.INTEGER
-        },
-        description:{
-            type: DataTypes.STRING
-        },
-        image:{
-            type: DataTypes.STRING
-        },
-        publication_date:{
-            type: DataTypes.DATE
-        },
-        isDelete:{
-          type: DataTypes.INTEGER,
-          defaultValue: 0
-        }
-      }
-    );
+  const Book = sequelize.define(
+    "book", // Model name
+    {
+      // Model attributes
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      author: {
+        type: DataTypes.STRING,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      publication_date: {
+        type: DataTypes.DATE,
+      },
+      isDelete: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+      },
+    }
+  );
 
-    return Book;
-  };
+  return Book;
+};
