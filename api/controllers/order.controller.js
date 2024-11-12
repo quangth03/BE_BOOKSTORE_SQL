0ba23e953da05de5941f8e3cfa358cea109429a4
QuangSang0810 <21110626@student.hcmute.edu.vn>
2024-11-12 15:43:00 +0700

    order

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -2,151 +2,176 @@ const db = require("../models");
 const { findAll } = require("./category.controller");
 
 module.exports = {
-    createOder : async (req, res) => {
-        if (!req.user_id) {
-            res.status(400).send({
-              message: "Content can not be empty!"
-            });
-            return;
-        }
+  createOder: async (req, res) => {
+    if (!req.user_id) {
+      res.status(400).send({
+        message: "Content can not be empty!",
+      });
+      return;
+    }
 
-        let cart = await db.cart.findOne({
-            where: {
-                user_id: req.user_id
-            }
-        })
+    let cart = await db.cart.findOne({
+      where: {
+        user_id: req.user_id,
+      },
+    });
 
-        if (cart != null) {
-            if (cart.total == 0) {
-                return res.status(400).send({
-                    message: "Cart is empty!"
-                });
-            }
+    if (cart != null) {
+      if (cart.total == 0) {
+        return res.status(400).send({
+          message: "Cart is empty!",
+        });
+      }
+    } else {
+      return res.status(400).send({
+        message: "Cart is empty!",
+      });
+    }
+
+    let order = await db.order.create({
+      user_id: req.user_id,
+      total: cart.total,
+      total_quantity: cart.total_quantity,
+    });
+
+    const cartIteam = await db.cart_details.findAll({
+      where: {
+        cart_id: cart.id,
+      },
+    });
+
+    cartIteam.forEach(async (item) => {
+      // Thêm chi tiết đơn hàng
+      await db.order_details.create({
+        order_id: order.id,
+        book_id: item.book_id,
+        quantity: item.quantity,
+        total: item.total,
+      });
+
+      // Cập nhật số lượng sản phẩm trong kho
+      let book = await db.books.findByPk(item.book_id);
+      if (book) {
+        await db.books.update(
+          { quantity: book.quantity - item.quantity }, // Trừ số lượng trong kho
+          { where: { id: item.book_id } }
+        );
+      }
+    });
+
+    try {
+      db.cart.destroy({
+        where: { id: cart.id },
+      });
+    } catch (error) {
+      return res.status(500).send({
+        message: err.message || "Some error occurred while destroy the cart.",
+      });
+    }
+
+    res.status(200).send(order);
+  },
+
+  deleteOrder: (req, res) => {
+    const id = req.params.id;
+
+    db.order
+      .destroy({
+        where: { id: id },
+      })
+      .then((num) => {
+        if (num == 1) {
+          res.send({
+            message: "Order was deleted successfully!",
+          });
         } else {
-            return res.status(400).send({
-                message: "Cart is empty!"
-            });
+          res.send({
+            message: `Cannot delete Order with id=${id}. Maybe Order was not found!`,
+          });
         }
-        
-        let order = await db.order.create({ user_id: req.user_id, total: cart.total, total_quantity: cart.total_quantity })
-        
-        const cartIteam = await db.cart_details.findAll({
-            where: {
-                cart_id: cart.id
-            }
-        })
-              
-        cartIteam.forEach(item => {
-            db.order_details.create({
-                order_id: order.id,
-                book_id: item.book_id,
-                quantity: item.quantity,
-                total: item.total
-            })
-        });
+      });
+  },
 
-        try {
-            db.cart.destroy({
-                where: { id: cart.id }            
-            })
-        } catch (error) {
-            return res.status(500).send({
-                message: err.message || "Some error occurred while destroy the cart."
-            });
+  getAllOder: (req, res) => {
+    db.order
+      .findAll()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while retrieving Order.",
+        });
+      });
+  },
+  updateOrder: (req, res) => {
+    let { id, ...data } = req.body;
+    db.order
+      .update(data, { where: { id: id } })
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while retrieving Order.",
+        });
+      });
+  },
+  getOders: async (req, res) => {
+    let user_id = null;
+    if (req.isAdmin) {
+      user_id = await req.params.id;
+    } else {
+      user_id = await req.user_id;
+    }
+
+    db.order
+      .findAll({
+        where: { user_id: user_id },
+      })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while retrieving Order.",
+        });
+      });
+  },
+
+  getOderDetails: (req, res) => {
+    db.order
+      .findOne({
+        where: { id: req.params.id },
+        include: [
+          {
+            model: db.books,
+            attributes: [
+              "id",
+              "title",
+              "author",
+              "price",
+              "description",
+              "publication_date",
+              "image",
+            ],
+            through: { attributes: ["quantity", "total"] },
+          },
+        ],
+      })
+      .then((data) => {
+        if (data) {
+          res.send(data);
+        } else {
+          res.status(400).send({
+            message: "Order not found!",
+          });
         }
-
-        res.status(200).send(order)
-
-    },
-
-    deleteOrder : (req, res) => {
-        const id = req.params.id;
-
-        db.order.destroy({
-            where: { id: id }
-        })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Order was deleted successfully!"
-                });
-            } else {
-                res.send({
-                    message: `Cannot delete Order with id=${id}. Maybe Order was not found!`
-                });
-            }
-        })
-    },
-
-    getAllOder: (req, res) => {
-        db.order.findAll()
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving Order."
-            });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while retrieving Order.",
         });
-    },
-    updateOrder: (req, res) => {
-        let { id, ...data } = req.body;
-        db.order.update(data, { where: { id: id } })
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving Order."
-            });
-        });
-    },
-    getOders : async (req, res) => {
-        let user_id = null
-        if( req.isAdmin ) {
-            user_id = await req.params.id
-        }else { 
-            user_id = await req.user_id 
-        }
-
-        db.order.findAll({
-            where: { user_id: user_id }
-        })
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving Order."
-            });
-        });
-    },
-
-    getOderDetails : (req, res) => {
-        db.order.findOne({
-            where: { id: req.params.id },
-            include: [{
-                model: db.books,
-                attributes : ['id', 'title', 'author', 'price', 'description', 'publication_date', 'image'], 
-                through: {attributes: ['quantity','total']}
-            }]
-        })
-        .then(data => {
-            if (data) {
-                res.send(data);
-            } else {
-                res.status(400).send({
-                    message: "Order not found!"              
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving Order."
-            });
-        });
-    },
-
-    
-
-}
+      });
+  },
+};
