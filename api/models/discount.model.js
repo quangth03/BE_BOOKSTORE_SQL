07e5347347cb40b12184356308f908ef81d04ee1
quangth03 <quangth2201@gmail.com>
2025-06-12 18:19:57 +0700

    change model discounts

--- api/models/discount.model.js
+++ api/models/discount.model.js
@@ -1,55 +1,56 @@
 module.exports = (sequelize, DataTypes) => {
-    const Discount = sequelize.define(
-      "Discount", // Tên model
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        value: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          validate: {
-            min: 0.01, // Giá trị giảm giá phải lớn hơn 0
-          },
-        },
-        description: {
-          type: DataTypes.STRING(255),
-          allowNull: true, // Có thể để trống
-        },
-        minimumOrderValue: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          defaultValue: 0.0, // Giá trị mặc định
-          validate: {
-            min: 0.0, // Không cho phép giá trị âm
-          },
-        },
-        createdAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: DataTypes.NOW, // Mặc định là thời gian hiện tại
-        },
-        updatedAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: DataTypes.NOW, // Mặc định là thời gian hiện tại
-        },
-        expiredAt: {
-          type: DataTypes.DATE,
-          allowNull: false, // Bắt buộc phải có giá trị
-          validate: {
-            isAfter: `${new Date().toISOString()}`, // Ngày hết hạn phải sau thời gian hiện tại
-          },
+  const Discount = sequelize.define(
+    "Discount", // Tên model
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      value: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 0.01, // Giá trị giảm giá phải lớn hơn 0
         },
       },
-      {
-        timestamps: true, // Tự động quản lý createdAt và updatedAt
-      }
-    );
-  
-    return Discount;
-  };
-  
+      description: {
+        type: DataTypes.STRING(255),
+        allowNull: true, // Có thể để trống
+      },
+      minimumOrderValue: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0.0, // Giá trị mặc định
+        validate: {
+          min: 0.0, // Không cho phép giá trị âm
+        },
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW, // Mặc định là thời gian hiện tại
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW, // Mặc định là thời gian hiện tại
+      },
+      expiredAt: {
+        type: DataTypes.DATE,
+        allowNull: false, // Bắt buộc phải có giá trị
+        validate: {
+          isAfter: `${new Date().toISOString()}`, // Ngày hết hạn phải sau thời gian hiện tại
+        },
+      },
+    },
+    {
+      timestamps: true, // Tự động quản lý createdAt và updatedAt
+
+      tableName: "discounts", // 👈 KHÔNG ĐƯỢC THIẾU
+    }
+  );
+
+  return Discount;
+};
